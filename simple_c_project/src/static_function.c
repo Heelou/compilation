641c399454cb21ec27095eabbb16d4f48a044b81
Heelou <89774691+Heelou@users.noreply.github.com>
2024-09-24 15:45:50 +0700

    Create static_function.c

---
+++ simple_c_project/src/static_function.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "../inc/Static_Function.h"
+
+
+int main(void) 
+{   
+    main_Basic();
+    main_Compound();
+    (void)square(1);
+    (void)Static_Function();
+    (void)Static_Function();
+    (void)Static_Function();
+    printf("STATIC FUNCTION!!!!!!!");
+
+}
