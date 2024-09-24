d415c8af3dc9e4bf8fc34dd903755e2d531f0d14
Heelou <89774691+Heelou@users.noreply.github.com>
2024-09-24 15:45:30 +0700

    Create compound_function.c

---
+++ simple_c_project/src/compound_function.c
@@ -0,0 +1,13 @@
+#include "../inc/Compound_Function.h"
+
+
+
+int main_Compound (void) 
+{
+    glob_int = 0;
+    compare_function();
+    (void)add_function(1,1);
+    (void)multiply_function(1,1);
+    printf("COMPOUND FUNCTION!!!!!!!");
+
+}
