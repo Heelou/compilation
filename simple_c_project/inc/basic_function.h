4d553840fc075091a519c0e979397bf458fb1aab
Heelou <89774691+Heelou@users.noreply.github.com>
2024-09-24 15:43:14 +0700

    Create basic_function.h

---
+++ simple_c_project/inc/basic_function.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#ifndef BASIC_FUNCTION_H_INCLUDED
+#define BASIC_FUNCTION_H_INCLUDED
+
+void print_function(void);
+void if_function(int a, int b);
+void for_function(int c);
+void pointer_function(int *d, int *e);
+
+
+#endif /* BASIC_FUNCTION_H_INCLUDED */
